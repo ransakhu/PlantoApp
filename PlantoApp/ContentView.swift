65d1b5451a1c5584242ee7f4ef000acc3a143040
ransakhu <ransakhu@gmail.com>
2025-10-19 14:25:55 +0300

    Initial Commit

---
+++ PlantoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantoApp
+//
+//  Created by rand on 19/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantoApp/PlantoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoAppApp.swift
+//  PlantoApp
+//
+//  Created by rand on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
